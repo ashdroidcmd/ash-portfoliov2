95f14f8574464c5f58ee571963907d747fb33726
ashdroidcmd <z.wolff25@gmail.com>
2025-05-26 14:19:19 +0800

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Hero from './pages/Hero'
 import About from './pages/About'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <>
+      <Analytics />
       <Navbar />
       <Hero />
       <About />
